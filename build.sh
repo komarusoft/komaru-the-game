da5e25e040ee41cfe79188a39e9e9591890f9a76
Yurij Boklan <hailrake@riseup.net>
2024-03-21 21:58:10 +0200

    initial commit

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ if [ -d "build" ]; then
     rm -rf build
 fi
 
+if [ -d "json"]; then
+    git clone https://github.com/nlohmann/json/
+fi
+
 mkdir build
 cd build
 cmake -DCMAKE_CXX_COMPILER=clang++ ..
